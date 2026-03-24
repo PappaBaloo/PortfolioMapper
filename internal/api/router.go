8f014df7fc68e207d18a1c7ce7db97573a283f37
PappaBaloo <charlienylund03@gmail.com>
2026-03-24 21:48:43 +0100

    using encoding/json instead

--- internal/api/router.go
+++ internal/api/router.go
@@ -1,9 +1,10 @@
 package api
 
 import (
-	"fmt"
 	"io"
 	"net/http"
+
+	"encoding/json"
 
 	"github.com/PappaBaloo/PortfolioMapper/internal/store"
 	"github.com/go-chi/chi/v5"
@@ -15,10 +16,13 @@ func NewRouter() *chi.Mux {
 		io.WriteString(w, "Neighbour!")
 	})
 	router.Get("/portfolio", func(w http.ResponseWriter, r *http.Request) {
-		portfolio := store.MockPortfolio()
-		response := fmt.Sprintf(`{"id":"%s","name":"%s","description":"%s"}`, portfolio.ID, portfolio.Name, portfolio.Description)
 		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte(response))
+		portfolio := store.MockPortfolio()
+		response := json.NewEncoder(w).Encode(portfolio)
+		if response != nil {
+			http.Error(w, "Failed to encode portfolio", http.StatusInternalServerError)
+			return
+		}
 	})
 	return router
 }
